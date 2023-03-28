40f334ddf3088a3c779b00df47803a71010c06da
EstevesTT <124747230+EstevesTT@users.noreply.github.com>
2023-03-27 23:06:29 -0300

    Delete DayToday-main directory

--- DayToday-main/src/main/java/br/com/fiap/daytoday/repository/MetaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fiap.daytoday.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fiap.daytoday.models.metas;
-
-public interface MetaRepository extends JpaRepository<metas, Long> {
-    
-}
